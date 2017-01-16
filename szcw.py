3278d073d794149901d0cb1f6863d3e94d961fa4
bg <bg@example.com>
2017-01-16 17:40:57 +0800

    to be fixed

--- szcw.py
+++ szcw.py
@@ -15,27 +15,46 @@
 # 5.偿还能力
 # 资产负债率,流动比率,速动比率
 
+import time
+from datetime import date
+from datetime import datetime, timedelta
 import pandas as pd
 
-watch_list = ['000001.XSHE']
+watch_list = ['000001.XSHE', '000002.XSHE']
 all_stcok = get_all_securities(['stock'])
-year_now = datetime.now().year
+now = datetime.now()  
+year_now = now.year
 
 q = query(
     income.code, income.operating_revenue, income.net_profit
 ).filter(
     valuation.code.in_(watch_list)
 )
-year_watch = [year_now]
-df = get_fundamentals(q, statDate='2015')
+year_watch = [year_now -1-i for i in range(5)]   # 2016 2015 2014
+df1 = [get_fundamentals(q, statDate=year_watch[i]) for i in range(5)]
 
 check_list = []
 y_danwei = 100000000
 for i in watch_list:
-    df_check = df[df.code==i]
-    check_list.append([i, all_stcok.ix[i].display_name.replace(' ', ''),round(df_check['operating_revenue'].values[0]/y_danwei,3), \
-    round(df_check['net_profit'].values[0]/y_danwei,3)])
+    tmp_list = []
+    df_check = df1[0][df1[0].code==i]
+    tmp_list.extend([i, all_stcok.ix[i].display_name.replace(' ', '')])
+    
+    if df1[0][df1[0].code==i].empty:
+        tmp_list.extend([0,0])
+    else:
+        tmp_list.extend([
+        round(df_check['operating_revenue'].values[0]/y_danwei,3), \
+        round(df_check['net_profit'].values[0]/y_danwei,3) ])
+    
+    
+    for j in range(1,5):            
+        df_check = df1[j][df1[j].code==i]
+        tmp_list.extend([round(df_check['operating_revenue'].values[0]/y_danwei,3), \
+        round(df_check['net_profit'].values[0]/y_danwei,3)])
+    check_list.append(tmp_list)
 
-columns=[u'code', u'名称', u'主收入（亿）', u'净利润（亿）']
+# to be fixed
+columns=[u'code', u'名称'] + [str(year_watch[i]-2000) for i in range(5)]+ [str(year_watch[i]-2000) for i in range(5)]
 df_watch= pd.DataFrame(data=check_list, columns=columns)
 df_watch
